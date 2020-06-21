cb8f8a52de64c563440fe01f126e4122095ef086
ngocdinh <ngocdinh2497@gmail.com>
2020-06-22 00:05:47 +0700

    update

---
+++ Assets/Scripts/Boomerang/Player/PlayerController.cs
@@ -0,0 +1,84 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    public float moveForce = 20f;
+    public float jumpForce = 700f;
+    public float maxVelocity = 4f;
+    private bool grounded;
+    private Rigidbody2D playerBody;
+    private Animator anim;
+
+    private void Awake()
+    {
+        playerBody = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    private void FixedUpdate()
+    {
+        PlayerMoveKeyBoard();
+    }
+
+    void PlayerMoveKeyBoard()
+    {
+        float forceX = 0f;
+        float forceY = 0f;
+        float vel = Mathf.Abs(playerBody.velocity.x);
+        float h = Input.GetAxisRaw("Horizontal");
+        if (h > 0)
+        {
+            if (vel < maxVelocity)
+                forceX = moveForce;
+
+            Vector3 scale = transform.localScale;
+            scale.x = 1f;
+            transform.localScale = scale;
+            anim.SetBool("isWalk", true);
+
+        }
+        else if (h < 0)
+        {
+            if (vel < maxVelocity)
+                forceX = -moveForce;
+
+            Vector3 scale = transform.localScale;
+            scale.x = -1f;
+            transform.localScale = scale;
+            anim.SetBool("isWalk", true);
+        }
+        else if (h == 0)
+        {
+            anim.SetBool("isWalk", false);
+        }
+
+        if (Input.GetKey(KeyCode.Space))
+        {
+            if (grounded)
+            {
+                grounded = false;
+                forceY = jumpForce;
+            }
+        }
+
+        playerBody.AddForce(new Vector2(forceX, forceY));
+    }
+
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.tag == "Ground")
+        {
+            grounded = true;
+        }
+    }
+
+}
---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    [SerializeField] private GameObject _cloudParticlePrefab;
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        Bird bird = other.collider.GetComponent<Bird>();
+        if (bird != null)
+        {
+            Instantiate(_cloudParticlePrefab, transform.position, Quaternion.identity);
+            Destroy(gameObject);
+            return;
+        }
+
+        Enemy enemy = other.collider.GetComponent<Enemy>();
+        if (enemy != null)
+        {
+            return;
+        }
+
+        if (other.contacts[0].normal.y < -0.5)
+        {
+            Instantiate(_cloudParticlePrefab, transform.position, Quaternion.identity);
+            Destroy(gameObject);
+        }
+    }
+}
